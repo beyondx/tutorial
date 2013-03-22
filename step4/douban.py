81e84da2006870f0acb038084de44fb04781e386
yoyicue <yoyicue@gmail.com>
2013-03-22 22:19:40 +0800

    [修改] 增加数据库的验证和去重

--- step4/douban.py
+++ step4/douban.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
+import os
+from datetime import datetime
+
 import requests
 from BeautifulSoup import BeautifulSoup
+
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
 
 def douban(city='beijing'):
     """
     访问豆瓣并返回北京正在上映的电影的的电影名称列表并存入 SQLite
     """
-
-    #数据库依赖
-    from sqlalchemy import Column, Integer, String
-    from sqlalchemy.ext.declarative import declarative_base
-    from sqlalchemy.orm import sessionmaker
-    from sqlalchemy import create_engine
 
     #数据库设置
     engine = create_engine('sqlite:///movie.sqlite')
@@ -26,6 +28,8 @@ def douban(city='beijing'):
 
         mid = Column(Integer, primary_key=True)
         name = Column(String(255))
+        create_time = Column(DateTime, default=datetime.now)
+
 
         def __init__(self, *args, **kwargs):
             super(Movie, self).__init__(*args, **kwargs)
@@ -33,12 +37,15 @@ def douban(city='beijing'):
         def __repr__(self):
             return '<Movie %s>' % self.id
 
-    #数据库初始化
-    #(并不是每次都需要, 待优化)
-    Base.metadata.create_all(engine)
+    #输出当前文件路径到 path
+    path = os.path.abspath(os.path.dirname(__file__))
+
+    #检查文件夹下是否有 movie.sqlite, 如果没有就创建并初始化数据库
+    if not os.path.isfile(os.path.join(path,'movie.sqlite')):
+        Base.metadata.create_all(engine)
 
     #GET请求
-    r = requests.get('http://movie.douban.com/nowplaying/%s/' %city)
+    r = requests.get('http://movie.douban.com/nowplaying/%s/' % city)
     print '访问结果: %i' %r.status_code
 
     #把网页内容实例化BeautifulSoup当中
@@ -47,17 +54,24 @@ def douban(city='beijing'):
     #取出来所有的 ll 的 span 标签 (内涵上映的电影名称)
     for movie in movie_list.findAll("span", { "class" : "ll" }):
 
-        #取出来 a 锚点的title 并去掉空格和换行
+        #取出来 a 锚点的 title 并去掉空格和换行
         name = ''.join(movie.findAll(text=True)).replace(' ', '').replace('\n', '')
 
         #实例化 Movie
         movie = Movie(name=name)
         #把 movie 存进 db
-        #(重复提交未判断, 待优化)
-        session.add(movie)
-        session.commit()
 
-        print '%s 已保存' %name.encode('utf-8')
+        #判断是否重复电影名
+        exists = session.query(Movie).filter(Movie.name == name).first()
+        if not exists:
+            #如果不重复保存数据库
+            session.add(movie)
+            session.commit()
+            print '%s 已保存' % name.encode('utf-8')
+        else:
+            #如果重复就跳过
+            print '%s 曾经已保存' % name.encode('utf-8')
+            pass
 
 
 if __name__ == '__main__':
